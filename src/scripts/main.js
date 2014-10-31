353cfa0791d74365f4e9ce4264ae923213b92d4d
Patrick Best <patrickbest@patrickbest.com>
2014-10-30 19:52:42 -0600

    add content, add js & edit markup/css
    > add artist images
    > add new background image
    > add release images
    > add scroll to & waypoints libs
    > add scroller nav
    > typography css
    > edit markup

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
+function l(data) {
+  console.log(data);
+}
+
 $(document).ready(function () {
 
 });
+
+$('.js-scroll').click(function() {
+  var target = $(this).data('scroll');
+/*   l(target); */
+  if (target === 'header') {
+    $('#header').ScrollTo()
+  } else if (target === 'footer') {
+    $('#footer').ScrollTo()
+  } else {
+    $('.artist').eq(target).ScrollTo();
+  }
+})
+
+$('.content-block').waypoint(function(direction) {
+  l(direction);
+}, { offset: '100%' }).waypoint(function(direction) {
+  l(direction);
+}, { offset: function() {
+  return -$(this).height();
+}});
